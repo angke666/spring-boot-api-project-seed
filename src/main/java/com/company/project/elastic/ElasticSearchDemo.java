9c4af327e8122672601145ad311efc359b2aa54a
qianjin <1556316829@qq.com>
2020-04-14 10:17:44 +0800

    学习elastic search

---
+++ src/main/java/com/company/project/elastic/ElasticSearchDemo.java
@@ -0,0 +1,11 @@
+package com.company.project.elastic;
+
+/**
+ * @ClassName ElasticSearchDemo
+ * @Description TODO
+ * @Author 钱进
+ * @Date 2020/4/14 10:16
+ * @Version 1.0
+ **/
+public class ElasticSearchDemo {
+}
